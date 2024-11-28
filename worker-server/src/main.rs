05b66f22bbbeb3727e39634270272f25602125d4
Cezar Crintea <cezar.crintea@gmail.com>
2024-11-28 09:45:08 +0200

    Feat: worker server

--- worker-server/src/main.rs
+++ worker-server/src/main.rs
@@ -1,4 +1,68 @@
-#[tokio::main]
-async fn main(){
+use std::net::SocketAddr;
 
+use bytes::Bytes;
+use http_body_util::{BodyExt, Full};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body::Incoming as IncomingBody, header, Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+use tokio::time::{sleep, Duration};
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            let service = service_fn(move |req| router(req));
+
+            if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
+}
+
+async fn router(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/health") => health_check().await,
+        (&Method::GET, "/work") => work().await,
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(full("Not Found"))
+            .unwrap()),
+    }
+}
+
+async fn health_check() -> Result<Response<BoxBody>> {
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "text/plain")
+        .body(full("OK"))?;
+    Ok(response)
+}
+
+async fn work() -> Result<Response<BoxBody>> {
+    sleep(Duration::from_millis(10)).await;
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "text/plain")
+        .body(full("Work done"))?;
+    Ok(response)
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
