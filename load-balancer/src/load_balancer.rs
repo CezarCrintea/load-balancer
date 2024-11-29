02c47e59554cb7ce349201fa34a03df83c2fddd6
Cezar Crintea <cezar.crintea@gmail.com>
2024-11-29 15:09:43 +0200

    feat: implement load balancer with round robin and least connections algorithms

---
+++ load-balancer/src/load_balancer.rs
@@ -0,0 +1,129 @@
+use std::{fmt, net::SocketAddr};
+
+pub struct Server {
+    address: String,
+    connections: usize,
+}
+
+impl Server {
+    pub fn new(address: String) -> Result<Self, String> {
+        if address.is_empty() {
+            return Err("Address cannot be empty".to_string());
+        }
+
+        if address.parse::<SocketAddr>().is_ok() {
+            Ok(Server {
+                address,
+                connections: 0,
+            })
+        } else {
+            Err(format!("Invalid address: {}", address))
+        }
+    }
+
+    pub fn get_address(&self) -> &str {
+        &self.address
+    }
+
+    pub fn get_connections(&self) -> usize {
+        self.connections
+    }
+
+    pub fn increment_connections(&mut self) {
+        self.connections += 1;
+    }
+
+    pub fn decrement_connections(&mut self) {
+        if self.connections > 0 {
+            self.connections -= 1;
+        }
+    }
+}
+
+const ROUND_ROBIN: &str = "round_robin";
+const LEAST_CONNECTIONS: &str = "least_connections";
+
+#[derive(Clone, Copy)]
+pub enum BalancingAlgorithm {
+    RoundRobin,
+    LeastConnections,
+}
+
+pub struct ConversionError;
+
+impl std::fmt::Display for ConversionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Invalid balancing algorithm")
+    }
+}
+
+impl TryFrom<&str> for BalancingAlgorithm {
+    type Error = ConversionError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            ROUND_ROBIN => Ok(BalancingAlgorithm::RoundRobin),
+            LEAST_CONNECTIONS => Ok(BalancingAlgorithm::LeastConnections),
+            _ => Err(ConversionError),
+        }
+    }
+}
+
+impl fmt::Display for BalancingAlgorithm {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self {
+            BalancingAlgorithm::RoundRobin => &ROUND_ROBIN,
+            BalancingAlgorithm::LeastConnections => &LEAST_CONNECTIONS,
+        };
+        write!(f, "{}", name)
+    }
+}
+
+pub struct LoadBalancer {
+    servers: Vec<Server>,
+    current_server: usize,
+    algorithm: BalancingAlgorithm,
+}
+
+impl LoadBalancer {
+    pub fn new(servers: Vec<Server>) -> Result<Self, String> {
+        if servers.is_empty() {
+            return Err("At least one server is required".to_string());
+        }
+
+        Ok(LoadBalancer {
+            servers,
+            current_server: 0,
+            algorithm: BalancingAlgorithm::RoundRobin,
+        })
+    }
+
+    pub fn next_server(&mut self) -> &Server {
+        match self.algorithm {
+            BalancingAlgorithm::RoundRobin => {
+                let servers_count = self.servers.len();
+                let server = &mut self.servers[self.current_server];
+                self.current_server = (self.current_server + 1) % servers_count;
+                server.increment_connections();
+                server
+            }
+            BalancingAlgorithm::LeastConnections => {
+                let server = self
+                    .servers
+                    .iter_mut()
+                    .min_by_key(|s| s.get_connections())
+                    .unwrap();
+                server.increment_connections();
+                server
+            }
+        }
+    }
+
+    pub fn set_algorithm(&mut self, algorithm: BalancingAlgorithm) {
+        self.algorithm = algorithm;
+    }
+
+    pub fn get_server_by_address(&mut self, address: &str) -> Option<&mut Server> {
+        self.servers.iter_mut().find(|s| s.get_address() == address)
+    }
+}
--- load-balancer/src/main.rs
+++ load-balancer/src/main.rs
@@ -1,4 +1,166 @@
-#[tokio::main]
-async fn main(){
+mod load_balancer;
 
+use std::env;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use bytes::{Buf, Bytes};
+use http_body_util::{BodyExt, Full};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::Uri;
+use hyper::{body::Incoming as IncomingBody, header, Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
+use load_balancer::{BalancingAlgorithm, LoadBalancer, Server};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::RwLock;
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let load_balancer = create_load_balancer().unwrap();
+    let load_balancer = Arc::new(RwLock::new(load_balancer));
+
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "80".to_string())
+        .parse::<u16>()?;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = TcpListener::bind(addr).await.map_err(|e| e.to_string())?;
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await.map_err(|e| e.to_string())?;
+        let io = TokioIo::new(stream);
+        let load_balancer_clone = load_balancer.clone();
+
+        tokio::task::spawn(async move {
+            let service = service_fn(move |req| handle_request(req, load_balancer_clone.clone()));
+
+            if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
+}
+
+fn create_load_balancer() -> Result<LoadBalancer> {
+    let servers = vec![
+        Server::new("127.0.0.1:3000".to_string())?,
+        Server::new("127.0.0.1:3001".to_string())?,
+        Server::new("127.0.0.1:3002".to_string())?,
+    ];
+
+    let lb = LoadBalancer::new(servers)?;
+    Ok(lb)
+}
+
+async fn handle_request(
+    req: Request<IncomingBody>,
+    lb: Arc<RwLock<LoadBalancer>>,
+) -> Result<Response<BoxBody>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::POST, "/algo") => change_algo(req, lb).await,
+        _ => forward_request(req, lb).await,
+    }
+}
+
+async fn change_algo(
+    req: Request<IncomingBody>,
+    lb: Arc<RwLock<LoadBalancer>>,
+) -> Result<Response<BoxBody>> {
+    let whole_body = req.collect().await?.aggregate();
+    let data: serde_json::Value = serde_json::from_reader(whole_body.reader())?;
+    if let Some(algo_value) = data.get("algo").and_then(|v| v.as_str()) {
+        match BalancingAlgorithm::try_from(algo_value) {
+            Ok(algo) => {
+                {
+                    let mut lb = lb.write().await;
+                    lb.set_algorithm(algo);
+                }
+
+                let response = Response::builder()
+                    .status(StatusCode::OK)
+                    .header(header::CONTENT_TYPE, "text/plain")
+                    .body(full(format!("Algorithm changed successfully to {}", algo)))?;
+                Ok(response)
+            }
+            Err(_) => {
+                let response = Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .header(header::CONTENT_TYPE, "text/plain")
+                    .body(full("Invalid algorithm value"))?;
+                Ok(response)
+            }
+        }
+    } else {
+        let response = Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .header(header::CONTENT_TYPE, "text/plain")
+            .body(full("Missing or invalid 'algo' key"))?;
+        Ok(response)
+    }
+}
+
+async fn forward_request(
+    req: Request<IncomingBody>,
+    lb: Arc<RwLock<LoadBalancer>>,
+) -> Result<Response<BoxBody>> {
+    let worker_addr = {
+        let mut lb = lb.write().await;
+        let server = lb.next_server();
+        server.get_address().to_string()
+    };
+
+    let worker_uri_string = format!(
+        "http://{}{}",
+        worker_addr,
+        req.uri()
+            .path_and_query()
+            .map(|x| x.as_str())
+            .unwrap_or("/")
+    );
+
+    let worker_uri = worker_uri_string.parse::<Uri>().expect("uri parse");
+
+    let headers = req.headers().clone();
+
+    let mut worker_req = Request::builder()
+        .method(req.method())
+        .uri(worker_uri)
+        .body(req.into_body())
+        .expect("request builder");
+
+    for (key, value) in headers.iter() {
+        worker_req.headers_mut().insert(key, value.clone());
+    }
+
+    let client_stream = TcpStream::connect(&worker_addr).await.unwrap();
+    let io = TokioIo::new(client_stream);
+
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    let worker_res = sender.send_request(worker_req).await?;
+    let res_body = worker_res.into_body().boxed();
+
+    {
+        let mut lb = lb.write().await;
+        let server = lb.get_server_by_address(&worker_addr).unwrap();
+        server.decrement_connections();
+    }
+
+    Ok(Response::new(res_body))
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
